0e8debff021c471a094ef96ec6ba6603e58d04a4
kemilly <okemilly89@gmail.com>
2022-02-28 17:35:18 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("                                         _ __");
+Console.WriteLine("        ___                             | '  \\");
+Console.WriteLine("   ___  \\ /  ___         ,'\\_           | .-. \\        /|");
+Console.WriteLine("   \\ /  | |,'__ \\  ,'\\_  |   \\          | | | |      ,' |_   /|");
+Console.WriteLine(" _ | |  | |\\/  \\ \\ |   \\ | |\\_|    _    | |_| |   _ '-. .-',' |_   _");
+Console.WriteLine("// | |  | |____| | | |\\_|| |__    //    |     | ,'_`. | | '-. .-',' `. ,'\\_");
+Console.WriteLine("\\\\_| |_,' .-, _  | | |   | |\\ \\  //    .| |\\_/ | / \\ || |   | | / |\\  \\|   \\");
+Console.WriteLine(" `-. .-'| |/ / | | | |   | | \\ \\//     |  |    | | | || |   | | | |_\\ || |\\_|");
+Console.WriteLine("   | |  | || \\_| | | |   /_\\  \\ /      | |`    | | | || |   | | | .---'| |");
+Console.WriteLine("   | |  | |\\___,_\\ /_\\ _      //       | |     | \\_/ || |   | | | |  /\\| |");
+Console.WriteLine("   /_\\  | |           //_____//       .||`      `._,' | |   | | \\ `-' /| |");
+Console.WriteLine("        /_\\           `------'        \\ |   AND        `.\\  | |  `._,' /_\\");
+Console.WriteLine("                                       \\|       THE          `.\\");
+Console.ReadKey();
+Console.WriteLine("                                            _  _  _  _  __ _  __ _ /_");
+Console.WriteLine("                                           (_`/ \\|_)/ '|_ |_)|_ |_)(_");
+Console.WriteLine("                                           ._)\\_/| \\\\_,|__| \\|__| \\ _)");
+Console.WriteLine("                                                           _ ___ _      _");
+Console.WriteLine("                                                          (_` | / \\|\\ ||__");
+Console.WriteLine("                                                          ._) | \\_/| \\||___");
+
+
+
+
+
